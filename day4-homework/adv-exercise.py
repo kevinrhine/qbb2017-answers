2c2667b74279b8682b0e77ce7086d7ca960bbde8
kevinrhine <rhine@jhu.edu>
2017-08-31 17:53:19 -0400

    advanced exercises completed

--- day4-homework/adv-exercise.py
+++ day4-homework/adv-exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Usage: ./01-timecourse.py <samples.csv> <samples_ctab_dir> <replicates.csv>
+Usage: ./01-timecourse.py <samples.csv> <samples_ctab_dir> <replicates.csv> <goi>
 
 - plot timecourse of FBtr0331261 for females
 """
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-transcript = "FBtr0331261"
+gene = sys.argv[4]
 
 df_samples = pd.read_csv(sys.argv[1])
 soi_f = df_samples["sex"] == "female"
@@ -21,8 +21,9 @@ fpkms_f = []
 for sample in df_samples["sample"][soi_f]:
     fname = os.path.join(sys.argv[2], sample, "t_data.ctab")
     df = pd.read_csv(fname, sep = "\t")
-    roi = df["t_name"] == transcript
-    fpkms_f.append(df[roi]["FPKM"])
+    roi = df["gene_name"] == gene
+    running_average = np.mean(df[roi]["FPKM"])
+    fpkms_f.append(running_average)
     
 soi_m = df_samples["sex"] == "male"
 
@@ -30,8 +31,9 @@ fpkms_m = []
 for sample in df_samples["sample"][soi_m]:
     mname = os.path.join(sys.argv[2], sample, "t_data.ctab")
     df = pd.read_csv(mname, sep = "\t")
-    roi = df["t_name"] == transcript
-    fpkms_m.append(df[roi]["FPKM"])
+    roi = df["gene_name"] == gene
+    running_average = np.mean(df[roi]["FPKM"])
+    fpkms_m.append(running_average)
     
 df_replicates = pd.read_csv(sys.argv[3])
 soi_rf = df_replicates["sex"] == "female"
@@ -40,8 +42,9 @@ fpkms_rf = [None, None, None, None]
 for replicate in df_replicates["sample"][soi_rf]:
     rfname = os.path.join(sys.argv[2], replicate, "t_data.ctab")
     df = pd.read_csv(rfname, sep = "\t")
-    roi_rf = df["t_name"] == transcript
-    fpkms_rf.append(df[roi_rf]["FPKM"])
+    roi = df["gene_name"] == gene
+    running_average = np.mean(df[roi]["FPKM"])
+    fpkms_rf.append(running_average)
     
 
 soi_rm = df_replicates["sex"] == "male"
@@ -50,8 +53,9 @@ fpkms_rm = [None, None, None, None]
 for replicate in df_replicates["sample"][soi_rm]:
     rmname = os.path.join(sys.argv[2], replicate, "t_data.ctab")
     df = pd.read_csv(rmname, sep = "\t")
-    roi_rm = df["t_name"] == transcript
-    fpkms_rm.append(df[roi_rm]["FPKM"])
+    roi = df["gene_name"] == gene
+    running_average = np.mean(df[roi]["FPKM"])
+    fpkms_rm.append(running_average)
     
 x1 = fpkms_f
 x2 = fpkms_m
@@ -69,8 +73,8 @@ plt.ylim(0,200)
 plt.xticks(range(len(fpkms_f)), df_samples["stage"])
 plt.xlabel("Developmental Stage", fontsize = 12)
 plt.ylabel("Relative Expression", fontsize = 12)
-plt.title("Gender-Biased Expression of " + str(transcript))
+plt.title("Gender-Biased Expression of " + str(gene))
 art =[]
 plt.legend(['females', 'males', 'female_replicates', 'male_replicates'], loc = "center right", bbox_to_anchor = (1.5, 0.5), frameon = False, numpoints = 1)
-plt.savefig( "timecourse.png", additional_artists = art, bbox_inches = "tight")
+plt.savefig(sys.argv[4] + "_timecourse.png", additional_artists = art, bbox_inches = "tight")
 plt.close()
